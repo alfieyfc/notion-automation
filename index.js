623a776f387edde82a8917795a49754107db79e0
alfieyfc <alfieyfc@github.com>
2021-08-22 01:23:15 +0800

    refactor: rm self defined n_api module & implement delete

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const dotenv = require("dotenv")
-
 dotenv.config()
 
+const { Client, LogLevel } = require("@notionhq/client")
 
 const env_mode = process.env.MODE
 const env_pages_path = process.env.PAGES_PATH
-const env_delete_path = process.env.DELETE_PATH
+const env_updates_path = process.env.UPDATES_PATH
 const env_users_path = process.env.USERS_PATH
 
 const attr = require("./tools/attributes")
 const utils = require("./tools/utils")
-const n_api = require("./tools/notion-api")
 
 // Authenticate Notion
-n_api.auth(process.env.NOTION_KEY, "DEBUG")
+const n_api = new Client({
+  auth: process.env.NOTION_KEY,
+  logLevel: LogLevel.WARN,
+})
 
 main()
 
@@ -24,15 +26,19 @@ async function main () {
     case ("create"):
       users = await utils.load_config(env_users_path)
       console.log("File user.json loaded.")
-      pages = await utils.load_config(env_pages_path)
+      objects = await utils.load_config(env_pages_path)
       console.log("File pages.json loaded.")
-      createPages(users, pages)
+      createPages(users, objects)
       break;
     case ("delete"):
-      deletePages(deletions)
+      deletion = await utils.load_config(env_updates_path)
+      console.log("File updates.json loaded.")
+      deletePages(deletion)
       break;
     case ("update"):
-      updatePages(pages)
+      objects = await utils.load_config(env_updates_path)
+      console.log("File updates.json loaded.")
+      updatePages(objects)
       break;
     default:
       console.log("Unrecognized MODE.")
@@ -40,26 +46,47 @@ async function main () {
   }
 }
 
-
-function createPages (users, objects) {
-  for (obj of objects) {
+function createPages (_users, _objects) {
+  for (obj of _objects) {
     let database = obj.database
     let pages = obj.pages
     for (page of pages) {
-      attr.generatePageObject(database, page, users)
+      attr.generatePageObject(database, page, _users)
         .then(async (result) => {
-          console.log(result)
-          n_api.createPage(result)
-          await new Promise(resolve => setTimeout(resolve, 1000));
+          // console.log(result)
+          n_api.pages.create(result)
+          await new Promise(resolve => setTimeout(resolve, 500));
         })
     }
   }
 }
 
-function deletePages (deletions) {
-  console.log(deletions)
+async function deletePages (_deletion) {
+  let databases = _deletion.databases
+  let filter = _deletion.filter
+  for (db of databases) {
+    n_api.databases.query({
+      database_id: db.id,
+      filter: filter
+    })
+      .then(async (result) => {
+        for (page of result.results) {
+          n_api.pages.update({
+            page_id: page.id,
+            archived: true
+          })
+          console.log("Archiving page: " + page.id)
+          await new Promise(resolve => setTimeout(resolve, 500));
+        }
+      }).catch(error => {
+        console.log(error.message)
+        process.exit(1);
+      })
+    console.log("Going through DB: " + db.id)
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
 }
 
-function updatePages (users, objects) {
+function updatePages (_objects) {
 
 }
